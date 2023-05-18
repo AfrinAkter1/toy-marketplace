e6b37ea996ddb74587eb1e9355853c7e74f0091a
Afrin Akter <akterafrin3600@gmail.com>
2023-05-18 07:52:55 +0600

    added error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <div className="mx-auto">
-           <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--_GqDpWw0--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/7aqcppklh6bexoa70320.jpg" alt="" />
-           <h3>Page not found</h3>
-           <Link className="btn">Back to Home</Link>
+        <div className="text-center">
+           <img className="ml-44" src="https://res.cloudinary.com/practicaldev/image/fetch/s--_GqDpWw0--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/7aqcppklh6bexoa70320.jpg" alt="" />
+           <h3 className="mb-5">Page not found</h3>
+           <Link to='/' className="btn">Back to Home</Link>
         </div>
     );
 };
